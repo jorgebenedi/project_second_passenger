9a741b724ed25b4e578bfdccaa4b88528ce99b67
Jorge Sánchez Benedi <141155996+jorgebenedi@users.noreply.github.com>
2024-07-16 23:55:43 +0200

    Update download.py

--- download.py
+++ download.py
@@ -27,8 +27,8 @@ def DownloadPostsPhotosVideos(IG,usernameToScrape):
     profile = Profile.from_username(IG.context,usernameToScrape)
     directoryUser = directoryDownload()
     
-    currentDir = os.getcwd() # guardamos directorio base
-    os.chdir(directoryUser) # cambiamos de directorio
+    currentDir = os.getcwd() 
+    os.chdir(directoryUser) 
     
     count = 0
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -40,7 +40,7 @@ def DownloadPostsPhotosVideos(IG,usernameToScrape):
                     print(f"{RED}[+]{RESET}{GREEN}--Downloading video number: {count}{RESET}")
                     IG.download_post(post, target=usernameToScrape)
                     count += 1
-                    if count % 5 == 0: # cada 5 publicaciones descargadas pausa de 5 segundos
+                    if count % 5 == 0: 
                         print(f"{RED}[WAITING]--Sleeping for 10 seconds{RESET}")
 
                         time.sleep(10)
@@ -55,7 +55,7 @@ def DownloadPostsPhotosVideos(IG,usernameToScrape):
                 IG.download_post(post, target=usernameToScrape)
                 
                 count += 1
-                if count % 5 == 0:  # cada 5 publicaciones descargadas pausa de 10 segundos
+                if count % 5 == 0:  
                     print(f"{RED}[WAITING]--Sleeping for 10 seconds{RESET}")
                     time.sleep(10)
         print(f"{RED}Finished downloading {name}{RESET}")
@@ -70,8 +70,7 @@ def DownloadPostsPhotosVideos(IG,usernameToScrape):
     return    
            
 def downloadVideos(IG,profile,usernameToScrape):
-    # de aqui lo quite
-    #postsSortedByLikes = sorted(profile.get_posts(), key=lambda post: post.likes, reverse=True)
+    
 
     print(f"\t{RED}[1]{RESET}{GREEN}--Download posts{RESET}")
     print(f"\t{RED}[2]{RESET}{GREEN}--Download posts photos or videos{RESET}")
@@ -90,7 +89,7 @@ def downloadVideos(IG,profile,usernameToScrape):
     if option == "2":
         DownloadPostsPhotosVideos(IG,usernameToScrape)
     if option == "3":
-        main() # llamamos a main() para regresar al menú principal
+        main() 
         return
         
                     
